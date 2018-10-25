99a4d78db87b42adaeffaebf4007c5a56ff5f2fe
Filipe Botti <04321613337@FCBNUDIGU181571.tj.ce.gov.br>
2018-10-25 16:46:52 -0300

    Main Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import  { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import  { faCheckSquare, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './components/login';
 import Register from './components/register';
+import Main from './components/main.js';
 
 
-library.add(faCheckSquare);
+library.add(faCheckSquare, faSignOutAlt);
 
 
 
@@ -19,6 +20,7 @@ class App extends Component {
         <div style={{ height: "100vh", width: "100vw" }}>
           <Route path="/" exact component={Login}/>        
           <Route path="/register" component={Register} />
+          <Route path="/main" component={Main}/>
         </div>
       </Router>
     );
---
+++ src/components/main.js/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Colors } from '../shared';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as appActions from '../../actions/app';
+
+// import TaskList from '../task-list';
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+`;
+
+const Header = styled.div`
+    height: 70px;
+    background-color: ${Colors.PURPLE};
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+    padding-left: 32px;
+    padding-right: 10px;
+    display: flex;
+`
+
+const Text = styled.span`
+    color: white;
+    font-size: 32px;
+    font-weight: bold;    
+    text-align: center;
+`;
+
+const SignOutButton = styled.button`
+    background-color: transparent;
+    outline: none;
+    border: none;    
+`;
+
+class Main extends React.Component {
+
+    constructor() {
+        super();
+
+        this.handleSignOut = this.handleSignOut.bind(this);
+    }
+
+    handleSignOut() {
+        // this.props.appActions.signOut();
+    }
+
+    render() {
+        return (
+            <Container>
+                <Header>                    
+                    <Text>Tasks</Text>
+                    <SignOutButton onClick={this.handleSignOut}>
+                        <FontAwesomeIcon icon="sign-out-alt" style={{color: 'white', fontSize: "22px"}}/>
+                    </SignOutButton>
+                </Header>
+                <div style={{flex:1}}>
+                    {/* <TaskList/> */}
+                </div>
+            </Container>
+        )
+    }
+}
+
+// export default connect(
+//     state => ({}),
+//     dispatch => ({
+//         appActions: bindActionCreators(appActions, dispatch)
+//     })
+// )(Main);
+export default Main;
